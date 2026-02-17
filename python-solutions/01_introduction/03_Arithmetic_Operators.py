396aa2f34119b72b412c759bd7b017a0d962347f
HarshvardhanAgrawal02 <historophile3100@gmail.com>
2026-02-17 22:04:41 +0530

    solution of 3rd problem

---
+++ python-solutions/01_introduction/03_Arithmetic_Operators.py
@@ -0,0 +1,9 @@
+# Problem: Arithmetic Operators
+# Difficulty: Easy
+# Link: https://www.hackerrank.com/challenges/python-arithmetic-operators/problem
+
+a= int(input().strip())
+b= int(input().strip())
+print(a+b)
+print(a-b)
+print(a*b)
